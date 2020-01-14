06e523cee89c9fefaa30e67f9e4fb2abf1360b5b
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-01-14 03:17:24 -0500

    Add files via upload

---
+++ gRPC-InspectPipeline/client.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader')
+
+const packageDefinition = protoLoader.loadSync('pps.proto');
+const proto = grpc.loadPackageDefinition(packageDefinition);
+
+const client = new proto.pps.API('0.0.0.0:30650', grpc.credentials.createInsecure())
+
+let request = {
+	'pipeline': {
+        'name': 'edges'
+    },
+
+        
+    'update': false
+}
+
+client.InspectPipeline(request, (error, response) => {
+  if(error) { return console.error(error) }
+  console.log(JSON.stringify(response, undefined, 4))
+})
